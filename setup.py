77de5722a6ca2ff54dce97c986d707ded5701071
Khalid Al-Amri <khalid.amri2000@gmail.com>
2025-07-04 16:51:03 +0300

    Add Description in PyPi

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="python_eicar",
-    version="0.1",
-    description="A python/pip package for testing antivirus detection for python packages and fasten testing using Eicar.",
+    version="0.1.0",
+    description="EICAR test string utility for AV detection testing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/khalidwalidalamri/python-eicar",
+    license="MIT",
     packages= find_packages(),
     install_requires=[],
     entry_points= {
