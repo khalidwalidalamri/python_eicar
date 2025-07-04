3e31b210656821678f78a6815c1d3f0c23f81a7d
Khalid Al-Amri <khalid.amri2000@gmail.com>
2025-07-04 17:01:02 +0300

    Push new version to PyPi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="python_eicar",
-    version="0.1.0",
+    version="0.1.1",
     description="EICAR test string utility for AV detection testing",
     long_description=long_description,
     long_description_content_type="text/markdown",
